0b0df4343cfffe41e9de1dc9ce7662dc98439d62
Maik Fröbe <maik44froebe@web.de>
2018-05-25 21:35:07 +0200

    Fix tests

--- tests/GameOfLifeTests.elm
+++ tests/GameOfLifeTests.elm
@@ -15,7 +15,7 @@ firstExampleTestSuite =
 
 
 testModel: Model.Model
-testModel = { width = 4, height = 4, livingCells = Set.empty, hoveredCell = ( -1, -1 ), history = [], presentationMode = False }
+testModel = { width = 4, height = 4, livingCells = Set.empty, hoveredCell = ( -1, -1 ), history = [], presentationMode = False, pausedBoardState = { width = 0, height = 0, livingCells = Set.empty, history = []} }
 
 utilityFunctionsSuite : Test.Test
 utilityFunctionsSuite =
